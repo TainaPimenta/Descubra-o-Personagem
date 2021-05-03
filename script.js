6ee02d5b740dc0e350d12a26f2f883d90d6274ad
TainaPimenta95 <81496791+TainaPimenta95@users.noreply.github.com>
2021-05-02 21:58:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+var tentativa = parseInt(prompt (
+"Digite 1 para Geralt,  2 para Cirila, 3 para Bardo e 4 para Yennefer"
+))
+
+const resposta = "4";
+
+if (tentativa == 4) {
+  alert ("Yeah! Yennefer!")
+} 
+
+if (tentativa == 1) {
+  alert ("Errou.")
+} 
+
+if (tentativa == 2) {
+  alert ("Errou.")
+} 
+
+if (tentativa == 3) {
+  alert ("Errou.")
+}
